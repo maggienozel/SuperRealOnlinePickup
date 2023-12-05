65ee5e86fb8a5c40779e66bb188a86b4f097bed3
Maggie Nozel <maggie.nozel@marquette.edu>
2023-12-05 13:33:03 -0600

    create order class

--- SuperRealOnlinePickup/Model/Order.cs
+++ SuperRealOnlinePickup/Model/Order.cs
@@ -8,6 +8,15 @@ namespace SuperRealOnlinePickup.Model
 {
     public class Order
     {
-        private static int 
+        private static int nextOrderNumber = 1;
+
+        public int OrderNumber { get; }
+        public Customer Customer { get; set; }
+        public string PickupTime { get; set; }
+        public int Quanity {  get; set; }
+
+        public List<Inventory> Item { get; } = new List<Inventory> ();
+
+        
     }
 }
